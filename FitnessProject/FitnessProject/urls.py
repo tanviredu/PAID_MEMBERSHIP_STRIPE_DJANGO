8460ddb58cf7c131a8c529a37fb298b8ef926154
tanviredu <ornobtanvir.git@gmail.com>
2020-07-07 22:07:30 +0600

    added

--- FitnessProject/FitnessProject/urls.py
+++ FitnessProject/FitnessProject/urls.py
@@ -8,11 +8,28 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('plans/<int:pk>',views.plan,name='plan'),
+    
+    # why this url is added
+    # because now you dont need to write any view
+    # or anyting for login,logout
+    # you just give the template
+    # and add the url
+    # and this will work through django administration
+    # watch the base.html to understand
+    # for login it wil automatically want a tempate
+    # inside the registration folder
+    # so you have to make a registration folder
+    # for this
     path('auth/',include('django.contrib.auth.urls')),
+    path('accounts/profile/',views.redi,name='redi'),
+    path('logout_user/',views.logout_user,name='logout_user'),
+    path('accounts/login/',views.red_to_login,name='red_to_login'),
+    
     path('auth/signup',views.SignUp.as_view(),name='signup'),
     path('join/',views.join,name="join"),
     path('checkout',views.checkout,name='checkout'),
     path('auth/settings',views.settings,name='settings'),
+    path('updateaccounts', views.updateaccounts, name='updateaccounts'),
 
 ]
 
